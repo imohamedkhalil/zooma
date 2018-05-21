2f47b2e8690d039617b84f34fd6cd252c70c46e8
aya-azb <yoya.azb@gmail.com>
2018-05-21 12:36:18 +0200

    animal listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomepageviewComponent } from './modules/homepage/homepageview/homepageview.component';
 import './modules/homepage/homepage.module.ts';
+import { ListingComponent } from 'src/app/modules/animals/listing/listing.component';
+import { AnimalitemComponent } from 'src/app/modules/animals/listing/animalitem/animalitem.component';
+import { SmallComponentComponent } from 'src/app/modules/animals/listing/small-component/small-component.component';
+import { FilterComponentComponent } from 'src/app/modules/animals/listing/filter-component/filter-component.component';
+import { AnimalService } from 'src/app/modules/animals/animal.service';
+import { OneSmallComponent } from 'src/app/modules/animals/listing/small-component/one-small-component/one-small.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageviewComponent
+    HomepageviewComponent,
+    ListingComponent,
+    AnimalitemComponent,
+    SmallComponentComponent,
+    FilterComponentComponent,
+    OneSmallComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'listing' , component: ListingComponent},
+      // {path: 'animals', component: SmallComponentComponent},
+      {path: 'details/:id', component: AnimalitemComponent}
+  ])
+],
+  providers: [AnimalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modules/animals/listing/animalitem/animal-disease/animal-disease.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-disease',
+  templateUrl: './animal-disease.component.html',
+  styleUrls: ['./animal-disease.component.css']
+})
+export class AnimalDiseaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

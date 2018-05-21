05adffc1a0819dd47e48ef61deb136c361cd4b78
gaber <mohamed95Gaber@gmail.com>
2018-05-21 12:00:03 +0200

    Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomepageviewComponent } from './modules/homepage/homepageview/homepageview.component';
-import './modules/homepage/homepage.module.ts';
+import { ZooComponent } from './modules/homepage/homepageview/zoos/zoo.component';
+import { ShowhomepageComponent } from './modules/homepage/homepageview/shows/showhomepage.component';
+import { NewshomepageComponent } from './modules/homepage/homepageview/news/newshomepage.component';
+import { AnimalhomepageComponent } from './modules/homepage/homepageview/animalcategouries/animalhomepage.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageviewComponent
+    HomepageviewComponent,
+    ZooComponent,
+    ShowhomepageComponent,
+    NewshomepageComponent,
+    AnimalhomepageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/interfaces/INewsHomePage.ts
@@ -0,0 +1,8 @@
+export interface INews{
+    imgUrl ?:string;
+    time? :DateTimeFormat;
+    location? :string;
+    title ?:string;
+    description? :string;
+    
+}

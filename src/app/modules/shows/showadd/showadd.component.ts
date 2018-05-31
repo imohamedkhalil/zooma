3f592d9b285d42076470452a619e2eb95fa8f2ec
gaber <mohamed95Gaber@gmail.com>
2018-05-31 13:37:37 +0200

    fixing edit show

--- src/app/modules/shows/showadd/showadd.component.ts
+++ src/app/modules/shows/showadd/showadd.component.ts
@@ -18,42 +18,28 @@ newShow:IShow;
   constructor(private showService:ShowService,private router:Router) { }
   ngOnInit() {
   }
-  // add(){
-  //  this.newShow={
-  //    imgUrl:this.imgUrl,
-  //    time:this.time,
-  //    location:this.location,
-  //    title:this.title,
-  //    description:this.description
-  //  }
-  //  console.log(this.newShow);
-  // }
-
-  // zooList:IZooSmallComponent[]=[];
   addShow(form){
     var show: IShow;
     show = form.value;
     console.log(show);
-    this.showService.shows.push(show);
-    this.router.navigate(['/show']);
-    this.imgUrl="";
-    this.time="";
-    this.location="";
-    this.title="";
-    this.description="";
+    if(show.title != undefined){
+      show.id =this.showService.shows.length+1;
+      this.showService.shows.push(show);
+       this.router.navigate(['/show']);
+    }   
+    this.imgUrl=undefined;
+    this.time=undefined;
+    this.location=undefined;
+    this.title=undefined;
+    this.description=undefined;
     console.log(this.showService.shows);
   }
   cancelShow(){
-    this.imgUrl="";
-    this.time="";
-    this.location="";
-    this.title="";
-    this.description="";
+    this.imgUrl=undefined;
+    this.time=undefined;
+    this.location=undefined;
+    this.title=undefined;
+    this.description=undefined;
   }
-
-
-
-
-
 
 }
